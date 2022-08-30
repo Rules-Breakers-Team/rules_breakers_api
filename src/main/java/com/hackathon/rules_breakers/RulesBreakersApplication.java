95b41c4aa72bb213220c9b9158e778e696856542
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2022-08-30 08:46:38 +0300

    chore: initial commit

---
+++ src/main/java/com/hackathon/rules_breakers/RulesBreakersApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.rules_breakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RulesBreakersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RulesBreakersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/rules_breakers/RulesBreakersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.rules_breakers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RulesBreakersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
