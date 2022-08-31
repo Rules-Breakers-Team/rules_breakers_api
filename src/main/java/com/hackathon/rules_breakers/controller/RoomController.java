83b4a4bd202d4488ff47fca917526c1aa193300c
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2022-08-31 15:43:50 +0300

    feat: put booking

---
+++ src/main/java/com/hackathon/rules_breakers/controller/RoomController.java
@@ -0,0 +1,12 @@
+package com.hackathon.rules_breakers.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("")
+public class RoomController {
+
+}
